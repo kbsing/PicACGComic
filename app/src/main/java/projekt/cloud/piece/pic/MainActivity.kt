d80ef86f17ef1607b5c5879d6c59aa0deeffc97b
1552980358 <1552980358@qq.com>
2022-11-19 16:44:30 +0800

    MainActivity.kt: Clean redundant imports

--- app/src/main/java/projekt/cloud/piece/pic/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/pic/MainActivity.kt
@@ -1,15 +1,8 @@
 package projekt.cloud.piece.pic
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import projekt.cloud.piece.pic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
