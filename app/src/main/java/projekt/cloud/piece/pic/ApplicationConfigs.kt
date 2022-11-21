d215c8df055c7743fcb6455b3ce2035039fd2118
1552980358 <1552980358@qq.com>
2022-11-21 15:28:03 +0800

    ApplicationConfigs: Update initializing account

--- app/src/main/java/projekt/cloud/piece/pic/ApplicationConfigs.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ApplicationConfigs.kt
@@ -1,5 +1,6 @@
 package projekt.cloud.piece.pic
 
+import android.content.Context
 import android.view.View
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -7,9 +8,37 @@ import androidx.core.view.WindowInsetsCompat.Type
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import projekt.cloud.piece.pic.api.ApiAuth.SignInResponseBody
+import projekt.cloud.piece.pic.api.ApiAuth.signIn
+import projekt.cloud.piece.pic.util.CoroutineUtil.io
+import projekt.cloud.piece.pic.util.CoroutineUtil.ui
+import projekt.cloud.piece.pic.util.HttpUtil.RESPONSE_CODE_SUCCESS
 import projekt.cloud.piece.pic.util.StorageUtil.Account
+import projekt.cloud.piece.pic.util.StorageUtil.readAccount
 
 class ApplicationConfigs: ViewModel() {
+
+    fun initializeAccount(context: Context) {
+        viewModelScope.launch(io) {
+            context.readAccount()?.let { account ->
+                ui { setAccount(account) }
+                signIn(account.account, account.password)?.let { response ->
+                    if (response.code == RESPONSE_CODE_SUCCESS) {
+                        ui {
+                            updateToken(
+                                Json.decodeFromString<SignInResponseBody>(response.body.string())
+                                    .token
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private val _windowInsetTop = MutableLiveData<Int>()
     val windowInsetTop: LiveData<Int>
@@ -35,7 +64,7 @@ class ApplicationConfigs: ViewModel() {
     }
 
     private val _token = MutableLiveData<String?>()
-    private val token: LiveData<String?>
+    val token: LiveData<String?>
         get() = _token
     fun updateToken(token: String?) {
         _token.value = token
