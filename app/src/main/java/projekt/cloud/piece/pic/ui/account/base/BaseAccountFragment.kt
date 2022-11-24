ce092b1e99874500a9722229cc6894e7ce1f9d09
1552980358 <1552980358@qq.com>
2022-11-24 13:49:02 +0800

    Update BaseAccountFragment

---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/account/base/BaseAccountFragment.kt
@@ -0,0 +1,26 @@
+package projekt.cloud.piece.pic.ui.account.base
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import projekt.cloud.piece.pic.R
+import projekt.cloud.piece.pic.ui.account.AccountFragment
+
+open class BaseAccountFragment: Fragment() {
+
+    private val accountFragment: AccountFragment
+        get() {
+            var containerFragment = requireParentFragment()
+            while (containerFragment !is AccountFragment) {
+                containerFragment = containerFragment.requireParentFragment()
+            }
+            return containerFragment
+        }
+
+    protected fun transactionTo(fragment: Fragment, enterTransition: Any) {
+        fragment.enterTransition = enterTransition
+        accountFragment.childFragmentManager.commit {
+            replace(R.id.fragment_container_view, fragment)
+        }
+    }
+
+}
