da5f99008b68447e6c315d1c30d61260bde15448
1552980358 <1552980358@qq.com>
2022-11-25 16:47:02 +0800

    Update CommonParam

---
+++ app/src/main/java/projekt/cloud/piece/pic/api/CommonParam.kt
@@ -0,0 +1,10 @@
+package projekt.cloud.piece.pic.api
+
+object CommonParam {
+
+    const val SORT_NEW_TO_OLD = "dd"
+    const val SORT_OLD_TO_NEW = "da"
+    const val SORT_MORE_HEART = "lv"
+    const val SORT_MORE_FAVOURITE = "vd"
+
+}
