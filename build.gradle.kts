13f9c0c1912bcc989be7745e40b6fcc6a60db5a3
1552980358 <1552980358@qq.com>
2022-12-01 15:39:56 +0800

    build.gradle.kts: Update Kotlin into 1.7.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.22" apply false
 }
 
 buildscript {
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.20")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
     }
 }
