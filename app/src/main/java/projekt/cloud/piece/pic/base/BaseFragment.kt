ecc382377092e06340724a58dd98483adca73b6b
1552980358 <1552980358@qq.com>
2022-12-02 13:58:47 +0800

    BaseFragment: Create applicationConfigs with activityViewModels()

--- app/src/main/java/projekt/cloud/piece/pic/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/base/BaseFragment.kt
@@ -1,13 +1,11 @@
 package projekt.cloud.piece.pic.base
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import projekt.cloud.piece.pic.ApplicationConfigs
 
 open class BaseFragment: Fragment() {
     
-    protected val applicationConfigs: ApplicationConfigs by viewModels(
-        ownerProducer = { requireActivity() }
-    )
+    protected val applicationConfigs: ApplicationConfigs by activityViewModels()
     
 }
