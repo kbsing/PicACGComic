995c806498490ec7b75e371a27e84ce676905fcf
1552980358 <1552980358@qq.com>
2022-12-03 16:42:49 +0800

    Update ComicContentFragment

---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/read/content/ComicContentFragment.kt
@@ -0,0 +1,53 @@
+package projekt.cloud.piece.pic.ui.read.content
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Job
+import projekt.cloud.piece.pic.Comic
+import projekt.cloud.piece.pic.api.ApiComics.EpisodeContentResponseBody.Data.Pages
+import projekt.cloud.piece.pic.api.ApiComics.EpisodeContentResponseBody.Data.Pages.Doc
+import projekt.cloud.piece.pic.base.BaseFragment
+import projekt.cloud.piece.pic.databinding.FragmentComicContentBinding
+import projekt.cloud.piece.pic.ui.read.ReadComic
+import projekt.cloud.piece.pic.ui.read.ReadFragment
+
+class ComicContentFragment: BaseFragment() {
+
+    private val readFragment: ReadFragment
+        get() = findParentAs()
+    
+    private val comic: Comic by activityViewModels()
+    
+    private val readComic: ReadComic by viewModels(
+        ownerProducer = { readFragment }
+    )
+    
+    private var _binding: FragmentComicContentBinding? = null
+    private val binding: FragmentComicContentBinding
+        get() = _binding!!
+    private val root get() = binding.root
+    private val recyclerView: RecyclerView
+        get() = binding.recyclerView
+    
+    private var job: Job? = null
+    
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentComicContentBinding.inflate(inflater, container, false)
+        return root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val pages = arrayListOf<Pages>()
+        val docs = arrayListOf<Doc>()
+        val images = mutableMapOf<String, Bitmap?>()
+        val recyclerViewAdapter = RecyclerViewAdapter(docs, images)
+        recyclerView.adapter = recyclerViewAdapter
+    }
+
+}
---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/read/content/RecyclerViewAdapter.kt
@@ -0,0 +1,63 @@
+package projekt.cloud.piece.pic.ui.read.content
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.withContext
+import projekt.cloud.piece.pic.api.ApiComics.EpisodeContentResponseBody.Data.Pages.Doc
+import projekt.cloud.piece.pic.databinding.LayoutRecyclerComicContentBinding
+import projekt.cloud.piece.pic.util.CoroutineUtil.io
+import projekt.cloud.piece.pic.util.CoroutineUtil.ui
+
+class RecyclerViewAdapter(private val docs: List<Doc>, private val images: MutableMap<String, Bitmap?>):
+    RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
+    
+    class RecyclerViewHolder(private val binding: LayoutRecyclerComicContentBinding): ViewHolder(binding.root) {
+        constructor(view: View): this(view.context)
+        constructor(context: Context): this(LayoutInflater.from(context))
+        constructor(layoutInflater: LayoutInflater): this(LayoutRecyclerComicContentBinding.inflate(layoutInflater))
+        
+        private var job: Job? = null
+        
+        fun setDoc(doc: Doc, images: MutableMap<String, Bitmap?>) {
+            val id = doc._id
+            when {
+                images.containsKey(id) -> binding.bitmap = images[id]
+                else -> {
+                    job?.cancel()
+                    job = ui {
+                        val image = withContext(io) {
+                            doc.media.bitmap
+                        }
+                        images[id] = image
+                        binding.bitmap = image
+                        job = null
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        RecyclerViewHolder(parent)
+    
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        holder.setDoc(docs[position], images)
+    }
+    
+    override fun getItemCount() = docs.size
+    
+    private var docSize = docs.size
+    
+    fun notifyListUpdate() {
+        notifyItemRangeInserted(docSize, docs.size)
+        docSize = docs.size
+    }
+    
+}
