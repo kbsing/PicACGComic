c4a004824d32a55f2dc70be0177c8e5fbbaad69a
1552980358 <1552980358@qq.com>
2022-12-03 16:54:19 +0800

    ReadFragment: Update ComicContentFragment with animation

--- app/src/main/java/projekt/cloud/piece/pic/ui/read/ReadFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/read/ReadFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.R
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentReadBinding
+import projekt.cloud.piece.pic.ui.read.content.ComicContentFragment
 
 class ReadFragment: BaseFragment() {
     
@@ -28,6 +32,8 @@ class ReadFragment: BaseFragment() {
         sharedElementEnterTransition = MaterialContainerTransform()
     }
     
+    private var currentContentFragment: Fragment? = null
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentReadBinding.inflate(inflater, container, false)
         
@@ -42,10 +48,26 @@ class ReadFragment: BaseFragment() {
     }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        updateContentFragment(requireAnimation = false)
         readComic.prev.observe(viewLifecycleOwner) {
+            updateContentFragment()
         }
         readComic.next.observe(viewLifecycleOwner) {
+            updateContentFragment(isForward =  false)
         }
+    }
+    
+    private fun updateContentFragment(fragment: Fragment = ComicContentFragment(),
+                                      requireAnimation: Boolean = true,
+                                      isForward: Boolean = true) {
+        if (requireAnimation) {
+            currentContentFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, isForward)
+            fragment.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, isForward)
+        }
+        childFragmentManager.commit {
+            replace(R.id.fragment_container_view, fragment)
+        }
+        currentContentFragment = fragment
     }
     
 }
