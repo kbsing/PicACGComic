2907a352bf4007cabfd4596434f9cb4dd70fbbff
1552980358 <1552980358@qq.com>
2022-12-05 20:41:38 +0800

    ComicContentFragment: Update page indicator

--- app/src/main/java/projekt/cloud/piece/pic/ui/read/content/ComicContentFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/read/content/ComicContentFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginBottom
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updateMargins
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import kotlinx.coroutines.withContext
 import okhttp3.Response
 import projekt.cloud.piece.pic.Comic
@@ -52,8 +58,12 @@ class ComicContentFragment: BaseFragment() {
         get() = binding.materialToolbar
     private val recyclerView: RecyclerView
         get() = binding.recyclerView
+    private val extendedFab: ExtendedFloatingActionButton
+        get() = binding.extendedFloatingActionButton
     
     private lateinit var navController: NavController
+    
+    private val docs = arrayListOf<Doc>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,10 +80,34 @@ class ComicContentFragment: BaseFragment() {
         toolbar.setupWithNavController(navController)
         
         val pages = arrayListOf<Pages>()
-        val docs = arrayListOf<Doc>()
         val images = mutableMapOf<String, Bitmap?>()
         val recyclerViewAdapter = RecyclerViewAdapter(lifecycleScope, docs, images)
         recyclerView.adapter = recyclerViewAdapter
+    
+        recyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            private fun canExtend(dy: Int) = dy <= 0
+        
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                updateExtendedFabText()
+                when {
+                    canExtend(dy) -> extendOrShrinkExtendedFab()
+                    else -> extendOrShrinkExtendedFab(false)
+                }
+            }
+        
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (!recyclerView.canScrollVertically(1) || !recyclerView.canScrollVertically(-1)) {
+                    extendOrShrinkExtendedFab()
+                }
+            }
+        })
+    
+        val extendedFabMarginBottom = extendedFab.marginBottom
+        applicationConfigs.windowInsetBottom.observe(viewLifecycleOwner) {
+            extendedFab.updateLayoutParams<CoordinatorLayout.LayoutParams> {
+                updateMargins(bottom = it + extendedFabMarginBottom)
+            }
+        }
         
         lifecycleScope.ui {
             val token = applicationConfigs.token.value ?: return@ui failed(R.string.request_not_logged)
@@ -102,6 +136,28 @@ class ComicContentFragment: BaseFragment() {
                     break
                 }
             }
+            updateExtendedFabText()
+        }
+    }
+    
+    private fun updateExtendedFabText() {
+        val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return
+        val firstPos = layoutManager.findFirstCompletelyVisibleItemPosition()
+        val lastPos = layoutManager.findLastCompletelyVisibleItemPosition()
+        val pos = when {
+            lastPos >= 0 -> lastPos
+            firstPos >= 0 -> firstPos
+            else -> layoutManager.findFirstVisibleItemPosition()
+        }
+        @Suppress("SetTextI18n")
+        extendedFab.text = "${pos + 1} / ${docs.size}"
+    }
+    
+    @Synchronized
+    private fun extendOrShrinkExtendedFab(extend: Boolean = true) {
+        when {
+            extend && !extendedFab.isExtended -> extendedFab.extend()
+            !extend && extendedFab.isExtended -> extendedFab.shrink()
         }
     }
     
